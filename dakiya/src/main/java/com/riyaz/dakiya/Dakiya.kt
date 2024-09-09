57d3610b2240cfb472ddee6765cd491e01b5c6ce
Riyaz Uddin <riyazuddin@dailyobjects.com>
2024-09-09 14:20:04 +0530

    Fixed core api

--- dakiya/src/main/java/com/riyaz/dakiya/Dakiya.kt
+++ dakiya/src/main/java/com/riyaz/dakiya/Dakiya.kt
@@ -1,6 +1,7 @@
 package com.riyaz.dakiya
 
 import android.content.Context
+import androidx.core.app.NotificationCompat
 import com.google.firebase.messaging.RemoteMessage
 import com.riyaz.dakiya.core.Constant.DAKIYA
 import com.riyaz.dakiya.core.NotificationProcessor
@@ -20,7 +21,7 @@ object Dakiya {
         return message.data.getOrNull(DAKIYA)?.toBoolean() == true
     }
 
-    fun processNotification(message: RemoteMessage){
-        NotificationProcessor.process(message)
+    fun processNotification(message: RemoteMessage): NotificationCompat.Builder {
+        return NotificationProcessor.process(message)
     }
 }
