f26daf6dbd4d3be59a943f3226036171e24ae3a7
Riyaz Uddin <riyazuddin@dailyobjects.com>
2024-09-11 16:59:41 +0530

    Added unit test for map extension function

--- dakiya/build.gradle.kts
+++ dakiya/build.gradle.kts
@@ -42,6 +42,7 @@ dependencies {
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.firebase.messaging.ktx)
+    implementation(libs.androidx.runner)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
---
+++ dakiya/src/test/java/com/riyaz/dakiya/core/util/UtilKtTest.kt
@@ -0,0 +1,30 @@
+package com.riyaz.dakiya.core.util
+
+import androidx.test.filters.SmallTest
+import org.junit.Assert
+import org.junit.Test
+
+
+@SmallTest
+class UtilKtTest{
+
+    private val data: Map<String, String> = mapOf(
+        "title" to "Big title",
+        "subtitle" to "small subtitle",
+        "image" to "old image"
+    )
+
+    @Test
+    fun `Map getOrNull(key) when key not present returns null`(){
+        //Querying key which is not present in the map
+        val value = data.getOrNull("tilt")
+        Assert.assertNull(value)
+    }
+
+    @Test
+    fun `Map getOrNull(key) when key is present returns value`(){
+        //Querying key which is not present in the map
+        val value = data.getOrNull("subtitle")
+        Assert.assertEquals("small subtitle", value)
+    }
+}
